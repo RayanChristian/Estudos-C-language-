824e1061e8e6313ab472ac8a6e2a6efc64194ba9
RayanChristian <rayanchrissilva@gmail.com>
2026-04-21 21:38:02 -0300

    Update F_to_C.c

--- F_to_C.c
+++ F_to_C.c
@@ -1,3 +1,7 @@
+/*Ler uma temperatura em graus Fahrenheit e apresentá-la convertida em graus Celsius. A fórmula de conversão de temperatura a 
+ser utilizada é C=((F-32)5)/9, em que a variável Fé a temperatura em graus Fahrenheit e a variável C é a temperatura em graus 
+Celsius.*/
+
 #include <stdio.h>
 int main(void)
 {
