c6d72dd80c4db1531cb8d903e871f5d69e971a78
RayanChristian <rayanchrissilva@gmail.com>
2026-04-21 21:41:49 -0300

    Add comments to AandB_value_exchange.c
    Added comments to explain the value exchange process between variables A and B.

--- AandB_value_exchange.c
+++ AandB_value_exchange.c
@@ -1,3 +1,7 @@
+/* Ler dois valores inteiros para as variáveis A e B, efetuar a troca dos valores de modo que 
+a variável A passe a possuir o valor da variável B, e a variável B passe a possuir o valor da variável A.
+Apresentar os valores trocados.*/
+
 #include <stdio.h>
 int main(){
 	int A,B,AUX;
