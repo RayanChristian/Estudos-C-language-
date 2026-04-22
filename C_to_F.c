1c72582fb8a920e0e669dce9e2a5455ee8ec0f7d
RayanChristian <rayanchrissilva@gmail.com>
2026-04-21 21:12:01 -0300

    Update C_to_F.c

--- C_to_F.c
+++ C_to_F.c
@@ -2,10 +2,10 @@
 int main(void)
 {
 	float C, F;
-	printf("\n\tDigite o valor da temperatura em Celsius: \n\t");
+	printf("\n\tDigite o valor da temperatura em Celsius: \n\t\t");
 	scanf("%f", &C);
 	F = ((9 * C + 160)/5);
-	printf("O valor em fahrenheit e: %.2f", F);
+	printf("\n\tO valor em fahrenheit e: %.2f\n\n", F);
 	
 	system("pause");
 	
